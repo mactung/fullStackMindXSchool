1b8bfd492a6a6b99798e04442be87e45b165bae4
mactungmindx <tungmt@mindxtech.com>
2020-01-15 09:56:54 +0700

    uodate

--- day1/main.js
+++ day1/main.js
@@ -4,12 +4,12 @@ let books = [
     {author: 'Suzanne Collins', title: 'Mockingjay: The FinalBook of The Hunger Games'}
 ]
 let searchInput = document.getElementById('searchInput')
-
 document.getElementById('btnSearch').onclick = ()=>{
+    document.getElementById('result').innerHTML = ''
     books.map(book => {
         if (book.author.toLowerCase().includes(searchInput.value.toLowerCase())
             || book.title.toLowerCase().includes(searchInput.value.toLowerCase())) 
-         {
+        {
              let {author, title} = book
             document.getElementById('result').innerHTML += `<div>Book: ${title}</div>
             <div>Author: ${author}</div>`
