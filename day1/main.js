46f348d290d1ed0e757350af1b6efeaac78f4b3f
mactungmindx <tungmt@mindxtech.com>
2020-01-15 09:48:38 +0700

    update day 1

---
+++ day1/main.js
@@ -0,0 +1,19 @@
+let books = [
+    { author: 'Bill Gates', title: 'The Road Ahead' },
+    { author: 'Steve Jobs', title: 'Walter Isaacson' },
+    {author: 'Suzanne Collins', title: 'Mockingjay: The FinalBook of The Hunger Games'}
+]
+let searchInput = document.getElementById('searchInput')
+
+document.getElementById('btnSearch').onclick = ()=>{
+    books.map(book => {
+        if (book.author.toLowerCase().includes(searchInput.value.toLowerCase())
+            || book.title.toLowerCase().includes(searchInput.value.toLowerCase())) 
+         {
+             let {author, title} = book
+            document.getElementById('result').innerHTML += `<div>Book: ${title}</div>
+            <div>Author: ${author}</div>`
+        }
+    })
+    
+}
